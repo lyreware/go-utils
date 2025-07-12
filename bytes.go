64b66aa933735bbac4d5b9a1f43d1f4eb5320c1d
rylenko <rylenko@tuta.io>
2025-07-12 21:41:34 +0700

    Use function signature variables

--- bytes.go
+++ bytes.go
@@ -1,19 +1,19 @@
 package utils
 
 // CloneByteSlice clones a slice of bytes, as slices.Clone would do, but does not allocate extra space.
-func CloneByteSlice(bytes []byte) []byte {
+func CloneByteSlice(bytes []byte) (clone []byte) {
 	if bytes == nil {
 		return nil
 	}
 
-	clone := make([]byte, len(bytes))
+	clone = make([]byte, len(bytes))
 	copy(clone, bytes)
 
 	return clone
 }
 
 // ConcatByteSlice concatenates a byte slices, as slices.Concat would do, but does not allocate extra space.
-func ConcatByteSlices(slices ...[]byte) []byte {
+func ConcatByteSlices(slices ...[]byte) (concat []byte) {
 	totalLen := 0
 	for _, slice := range slices {
 		totalLen += len(slice)
@@ -23,7 +23,7 @@ func ConcatByteSlices(slices ...[]byte) []byte {
 		return nil
 	}
 
-	concat := make([]byte, 0, totalLen)
+	concat = make([]byte, 0, totalLen)
 	for _, slice := range slices {
 		concat = append(concat, slice...)
 	}
--- tx.go
+++ tx.go
@@ -7,8 +7,9 @@ type TxFunc[T any] func(dirty *T) error
 // UpdateWithTx passes the dirty value to the transaction function and, if the transaction occurred without errors,
 // replaces the target with the dirty version. This is useful for use in public methods to avoid corrupting the state
 // with errors.
-func UpdateWithTx[T any](target *T, dirty T, tx TxFunc[T]) error {
-	if err := tx(&dirty); err != nil {
+func UpdateWithTx[T any](target *T, dirty T, tx TxFunc[T]) (err error) {
+	err = tx(&dirty)
+	if err != nil {
 		return err
 	}
 
@@ -18,5 +19,5 @@ func UpdateWithTx[T any](target *T, dirty T, tx TxFunc[T]) error {
 
 	*target = dirty
 
-	return nil
+	return err
 }
